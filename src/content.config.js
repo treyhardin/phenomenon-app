62311787f58834ba6b2653177864dde68b4e9b0a
Trey Hardin <hello@trumancreative.co>
2025-04-27 16:17:10 -0400

    Initial Commit

---
+++ src/content.config.js
@@ -0,0 +1,41 @@
+import { defineCollection, z } from 'astro:content';
+import { supabase } from './lib/supabase';
+
+const countries = defineCollection({
+  loader: async () => {
+
+    let { data: countries, error } = await supabase
+      .from('Lookup Country')
+      .select('*')
+      .gt('record_count', 0)
+      .order('record_count', { ascending: false })
+
+    return countries.map((country) => ({ id: country.id, ...country }))
+  }
+});
+
+const statesByCountry = defineCollection({
+  loader: async () => {
+
+    let { data: entries, error } = await supabase
+      .from('states_by_country')
+      .select('*')
+      .order('country', { ascending: false })
+
+    return entries.map((entry) => ({ id: entry.country, ...entry }))
+  }
+});
+
+const citiesByState = defineCollection({
+  loader: async () => {
+
+    let { data: entries, error } = await supabase
+      .from('cities_by_state')
+      .select('*')
+      .order('state', { ascending: false })
+
+    return entries.map((entry) => ({ id: entry.state, ...entry }))
+  }
+});
+
+export const collections = { countries, statesByCountry, citiesByState };
