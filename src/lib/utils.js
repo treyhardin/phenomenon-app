5528d0ab10436e8053f8ffa0f92bbe8a59a080b1
Trey Hardin <trey@meetdomaine.com>
2025-05-15 09:50:59 -0400

    Try add images

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,3 +3,29 @@ export const formatDate = (date) => {
   const formattedDate = `${(dateObject.getMonth() + 1).toString().padStart(2, '0')}.${dateObject.getDate().toString().padStart(2, '0')}.${dateObject.getFullYear()}`;
   return formattedDate
 }
+
+export const checkIsImage = (url) => {
+  const imageExtensions = ['.gif', '.jpg', '.jpeg', '.png'] // you can add more extensions
+
+  let isImage
+
+  imageExtensions.map((e) => {
+    if (url.includes(e)) return isImage = true;
+  })
+
+  return isImage
+
+}
+
+export const checkIsVideo = (url) => {
+
+  const videoExtensions = ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mov', '.mp4'] //you can add more extensions
+
+  let isVideo = false
+  
+  videoExtensions.map((e) => {
+    if (url.toLowerCase().includes(e)) return isVideo = true
+  })
+
+  return isVideo
+}
