0e74ffcb67725160ce411d972f9f1525f3c346c4
Trey Hardin <hello@trumancreative.co>
2025-06-14 14:56:19 -0400

    Repush astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import solidJs from '@astrojs/solid-js';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
