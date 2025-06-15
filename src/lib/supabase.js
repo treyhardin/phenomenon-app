56324cdbfc29eb85ddb6916bc4f2e6d2226d35ca
Trey Hardin <hello@trumancreative.co>
2025-06-15 11:22:52 -0400

    Try vite prefix

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,6 +3,8 @@ import { loadEnv } from 'vite';
 
 const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
+console.log(`SUPABASE: env.VITE_TEST_ENV`)
+
 export const supabase = createClient(
   env.SUPABASE_URL, 
   env.SUPABASE_PUBLIC_ANON_KEY
