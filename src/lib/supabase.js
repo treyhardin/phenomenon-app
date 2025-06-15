8f7572bcc00b91bf4c77d6171fd5f3a81a0f32c2
Trey Hardin <hello@trumancreative.co>
2025-06-15 11:24:05 -0400

    I'm an idiot

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,7 +3,7 @@ import { loadEnv } from 'vite';
 
 const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
-console.log(`SUPABASE: env.VITE_TEST_ENV`)
+console.log(`SUPABASE: ${env.VITE_TEST_ENV}`)
 
 export const supabase = createClient(
   env.SUPABASE_URL, 
