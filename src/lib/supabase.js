6ed3881a96b0d6bfb6569e42d93b88e67b56c262
Trey Hardin <hello@trumancreative.co>
2025-06-15 11:25:41 -0400

    Hail mary

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,7 +3,7 @@ import { loadEnv } from 'vite';
 
 const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
-console.log(`SUPABASE: ${env.VITE_TEST_ENV}`)
+console.log(`PLEASE WORKKKK: ${env.VITE_TEST_ENV || process.env.VITE_TEST_ENV || import.meta.env.VITE_TEST_ENV}`)
 
 export const supabase = createClient(
   env.SUPABASE_URL, 
