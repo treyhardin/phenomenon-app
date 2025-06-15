f0688758d2afd621fbb494620de30f0bc509460e
Trey Hardin <hello@trumancreative.co>
2025-06-15 12:12:24 -0400

    R2 queries env

--- src/lib/r2-queries.js
+++ src/lib/r2-queries.js
@@ -1,21 +1,18 @@
 import { GetObjectCommand, HeadObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { loadEnv } from 'vite';
-
-const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 export const S3 = new S3Client({
 	region: "auto",
-	endpoint: `https://${env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+	endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
 	credentials: {
-		accessKeyId: env.CLOUDFLARE_ACCESS_KEY_ID,
-		secretAccessKey: env.CLOUDFLARE_SECRET_ACCESS_KEY,
+		accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID,
+		secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY,
 	},
 });
 
 export const getSignedMediaUrl = async (key, expiresIn = 604800) => {
   const command = new GetObjectCommand({
-    Bucket: env.CLOUDFLARE_BUCKET,
+    Bucket: process.env.CLOUDFLARE_BUCKET,
     Key: key,
   });
   const signedUrl = await getSignedUrl(S3, command, { expiresIn });
@@ -24,7 +21,7 @@ export const getSignedMediaUrl = async (key, expiresIn = 604800) => {
 
 export const getMimeTypeForKey = async (key) => {
   const command = new HeadObjectCommand({
-    Bucket: env.CLOUDFLARE_BUCKET,
+    Bucket: process.env.CLOUDFLARE_BUCKET,
     Key: key,
   });
 
